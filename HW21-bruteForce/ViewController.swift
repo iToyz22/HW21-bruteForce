73be90497b26b60a70574abb13b60b11278b523b
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-07-30 08:50:05 +0300

    Initial Commit

---
+++ HW21-bruteForce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21-bruteForce
+//
+//  Created by Anatoliy on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
